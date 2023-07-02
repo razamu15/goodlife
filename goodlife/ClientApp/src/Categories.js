5b940cdf2692fbf285d6ee0ac550b7cf2ba84ffa
razamu15 <m.saad.raza1999@gmail.com>
2023-07-01 20:28:59 -0400

    did backend call to controller route

--- goodlife/ClientApp/src/Categories.js
+++ goodlife/ClientApp/src/Categories.js
@@ -1,10 +1,12 @@
-﻿
+﻿import { useState, useEffect } from 'react'
+
 //import categories from './categories-data'
 
 // const API_URL = 'https://gldev-practicalapi.azurewebsites.net/api/FitnessClass/GetCategories'
 
 
 export function Categories() {
+    const [state, setState] = useState({ forecasts: [], loading: true })
     // const [data, setData] = useState([])
 
     // const callAPI = async () => {
@@ -13,6 +15,15 @@ export function Categories() {
     //   console.log(res.body)
     //   setData(res.json())
     // }
+    const populateWeatherData = async () => {
+        const response = await fetch('weatherforecast');
+        const data = await response.json();
+        setState({ forecasts: data, loading: false });
+    }
+
+    useEffect(() => {
+        populateWeatherData()
+    }, [])
 
 
     // if (error) return <div>failed to load</div>
@@ -20,15 +31,31 @@ export function Categories() {
     // if (data.length === 0) return <div>loadindg ... </div>
     let categories = [{ id: 1, name: 'yes' }]
 
+    if (state.loading) {
+        return( <p><em>Loading...</em></p>)
+    }
+        
+
     return (
         <div className='h-96 w-96 ring-1 ring-inset ring-gray-500'>
-            {categories.map((cat) => {
-                return <>
-                    <span>{cat.id}</span>
-                    <span>{cat.name}</span>
+            {state.forecasts.map(forecast =>
+                <tr key={forecast.date}>
+                    <td>{forecast.date}</td>
+                    <td>{forecast.temperatureC}</td>
+                    <td>{forecast.temperatureF}</td>
+                    <td>{forecast.summary}</td>
+                </tr>
+            )}
 
-                </>
-            })}
+            {/*{categories.map((cat) => {*/}
+            {/*    return <>*/}
+            {/*        <span>{cat.id}</span>*/}
+            {/*        <span>{cat.name}</span>*/}
+
+
+
+            {/*    </>*/}
+            {/*})}*/}
         </div>
     )
 }
