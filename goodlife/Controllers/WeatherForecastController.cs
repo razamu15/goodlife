9185c23a4438337dd4d96977770e85ea0dc60882
razamu15 <m.saad.raza1999@gmail.com>
2023-07-02 10:26:57 -0400

    removed more old shit, now just gotta work

--- goodlife/Controllers/WeatherForecastController.cs
+++ goodlife/Controllers/WeatherForecastController.cs
@@ -7,33 +7,21 @@ namespace goodlife.Controllers;
 [Route("[controller]")]
 public class WeatherForecastController : ControllerBase
 {
-    static readonly HttpClient client = new HttpClient();   
-    private static readonly string[] Summaries = new[]
-    {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
+    static readonly HttpClient client = new HttpClient();
 
     private readonly ILogger<WeatherForecastController> _logger;
 
-    public WeatherForecastController(ILogger<WeatherForecastController> logger)
-    {
+    public WeatherForecastController(ILogger<WeatherForecastController> logger) {
         _logger = logger;
     }
 
     [HttpGet]
-    public async Task<IEnumerable<Category>?>  Get()
-    {
+    public async Task<IEnumerable<Category>?> Get() {
         var uri = "https://gldev-practicalapi.azurewebsites.net/api/FitnessClass/GetCategories";
-
-        // { id: 1, name: '' }
 
         using HttpResponseMessage response = await client.GetAsync(uri);
         response.EnsureSuccessStatusCode();
         var responseBody = await response.Content.ReadFromJsonAsync<IEnumerable<Category>>();
-        // Above three lines can be replaced with new helper method below
-        // string responseBody = await client.GetStringAsync(uri);
-
-        //Console.WriteLine(responseBody);
         return responseBody;
     }
 }
